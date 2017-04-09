5af845cfc50b50b9931ee4ca19446da18edf469a
Joseph North <north@sublink.ca>
2017-04-08 23:05:02 -0700

    Remove the use of Symbol-based interators.
    This increases compatability, and reduces file size. Win-win!

--- dist/index.es.js
+++ dist/index.es.js
@@ -198,29 +198,9 @@ var Store = function () {
         after: after
       };
 
-      var _iteratorNormalCompletion = true;
-      var _didIteratorError = false;
-      var _iteratorError = undefined;
-
-      try {
-        for (var _iterator = this[$subscribers][Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-          var callback = _step.value;
-          callback(event);
-        }
-      } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
-      } finally {
-        try {
-          if (!_iteratorNormalCompletion && _iterator.return) {
-            _iterator.return();
-          }
-        } finally {
-          if (_didIteratorError) {
-            throw _iteratorError;
-          }
-        }
-      }
+      this[$subscribers].forEach(function (subscriber) {
+        return subscriber(event);
+      });
 
       return after;
     }
